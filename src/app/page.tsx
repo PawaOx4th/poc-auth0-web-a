74e704076d773ecd1a41b4b0f78d0f1c65c3abdc
Kaen Pawanachai <pawaox4th@gmail.com>
2024-09-20 18:34:51 +0700

    feat(app): defined theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+
 export default function Home() {
 	return (
-		<div>
-			<h1>Hello, world!</h1>
-		</div>
+		<Container className="min-h-screen flex items-center justify-center">
+			<Card
+				sx={{
+					padding: "2rem",
+				}}
+			>
+				<Stack
+					spacing={2}
+					direction="column"
+					className="w-[500px] mx-auto my-auto "
+				>
+					<Button variant="text">Text</Button>
+					<Button variant="contained">Contained</Button>
+					<Button variant="outlined">Outlined</Button>
+				</Stack>
+			</Card>
+		</Container>
 	);
 }
--- src/theme.ts
+++ src/theme.ts
@@ -5,6 +5,11 @@ const theme = createTheme({
   typography: {
     fontFamily: 'var(--font-montserrat)',
   },
+  palette : {
+    primary: {
+      main : "#18181b"
+    }
+  }
 });
 
 export default theme;
