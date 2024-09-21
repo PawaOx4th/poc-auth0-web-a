3b3418cec4ef6f5549697ecaa9741cb3d201c9ee
Kaen Pawanachai <pawaox4th@gmail.com>
2024-09-21 23:10:09 +0700

    fix(app): remove code unused

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,10 +1,8 @@
-import { cookieManager } from "@/helper/manage-cookie";
 import { validateKey } from "@/helper/validate-token";
-import { NextApiRequest, NextApiResponse } from "next";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
-export const GET = async (req: NextApiRequest) => {
+export const GET = async () => {
 	// Solution 1.
 	// const token = cookieManager().get("token");
 	// console.log(`[LOG] 🟡  token :`, token);
